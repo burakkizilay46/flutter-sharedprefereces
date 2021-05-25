05b1bcd2e524100a5294d12c025b6c6ee3c55496
Burak Kızılay <40871181+burakkizilay46@users.noreply.github.com>
2021-05-25 16:36:12 +0300

    CounterApp Done!

--- lib/screens/sayaac_sp.dart
+++ lib/screens/sayaac_sp.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SayacPage extends StatelessWidget {
+class SayacPage extends StatefulWidget {
+  
+  @override
+  _SayacPageState createState() => _SayacPageState();
+}
+
+class _SayacPageState extends State<SayacPage> {
+  late int _counter;
+
+  Future<void> sharedPreferencesHazirla() async{
+    var sharedPreferences = await SharedPreferences.getInstance();
+    _counter = sharedPreferences.getInt("sayac") ?? 0;
+    setState(() {});
+  }
+
+  Future<void> veriKaydet() async {
+    var sharedPreferences = await SharedPreferences.getInstance();
+    _counter += 1;
+    sharedPreferences.setInt("sayac", _counter);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    sharedPreferencesHazirla();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("data"),
+          child: Text("$_counter" , style: TextStyle(
+            fontSize: 32,
+          ),),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){},
+          onPressed: (){
+            setState(() {
+              veriKaydet();
+            });
+          },
           child: Icon(Icons.add),
         ),
       ),
